3ceb94c95ac259ec31a018e0b016e09b7cc97463
cnjszzw <cnjszzw@qq.com>
2024-11-04 18:33:57 +0800

    BUG-FIX.增加跨域配置

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,14 @@ const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
-        host: 'localhost',
-        port: 7070
+        // host: 'localhost',
+        host: '0.0.0.0',
+        port: 7070, // 保持前端在7070端口
+        allowedHosts: [
+            'localhost',
+            'citywork.icu',
+            'www.citywork.icu'
+            // 其他允许的主机名
+        ]
     }
 })
